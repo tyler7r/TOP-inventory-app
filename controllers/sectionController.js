c20792c3f8a9ee2dda3ac6ea983c7e1aab7a82b0
Tyler Randall <tyler7r@gmail.com>
2023-05-03 12:27:51 -0400

    Add section_detail and section_list pages

--- controllers/sectionController.js
+++ controllers/sectionController.js
@@ -1,12 +1,33 @@
 const Section = require('../models/section');
+const Product = require('../models/product')
 const asyncHandler = require('express-async-handler');
 
 exports.section_list = asyncHandler(async (req, res, next) => {
+    const sections = await Section.find().exec();
 
+    res.render('section_list', {
+        title: "Browse Sections",
+        section_list: sections,
+    })
 })
 
 exports.section_detail = asyncHandler(async (req, res, next) => {
+    const [section, productsInSection] = await Promise.all([
+        Section.findById(req.params.id).exec(),
+        Product.find({ section: req.params.id }, "name description").exec() 
+    ])
 
+    if (section === null) {
+        const err = new Error("Section not found")
+        err.status = 404
+        return next(err)
+    }
+
+    res.render('section_detail', {
+        title: `${section.name}'s Section`,
+        section: section.name,
+        section_products: productsInSection,
+    })
 })
 
 exports.section_create_get = asyncHandler(async (req, res, next) => {
